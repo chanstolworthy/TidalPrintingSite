3f8922cb93da3f4fbbbfefac1b73f7fc7e007eb2
Chandler Stolworthy <chandlerstolworthy@Chandler-Stolworthys-MacBook-Pro.local>
2019-08-23 08:28:39 -0600

    Updates

---
+++ precache-manifest.d7bac79e8a275491f7f5f726815e7e4c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbde5edbb8dc47cb35852a6bd14ad04",
+    "url": "/TidalPrintingSite/index.html"
+  },
+  {
+    "revision": "b22fb888fd4ec70e7e64",
+    "url": "/TidalPrintingSite/static/css/main.c59db92c.chunk.css"
+  },
+  {
+    "revision": "eea80360d6b4a4f9c78b",
+    "url": "/TidalPrintingSite/static/js/2.00c2dfff.chunk.js"
+  },
+  {
+    "revision": "b22fb888fd4ec70e7e64",
+    "url": "/TidalPrintingSite/static/js/main.805c8037.chunk.js"
+  },
+  {
+    "revision": "8a039b6cf5b0f64f22ba",
+    "url": "/TidalPrintingSite/static/js/runtime~main.d21b5c2e.js"
+  },
+  {
+    "revision": "3cbee4eb9afc216455d014f08ba9a56a",
+    "url": "/TidalPrintingSite/static/media/background_left.3cbee4eb.png"
+  },
+  {
+    "revision": "dbf52c0d02e15dfb202f1dd79e23606a",
+    "url": "/TidalPrintingSite/static/media/background_right.dbf52c0d.png"
+  },
+  {
+    "revision": "d2affb17923bb11d0b58d1d93dace937",
+    "url": "/TidalPrintingSite/static/media/hero.d2affb17.png"
+  },
+  {
+    "revision": "d14334dc56af4d7bd7bf7834c9f394af",
+    "url": "/TidalPrintingSite/static/media/map.d14334dc.png"
+  },
+  {
+    "revision": "223f5d48d2fc31b5623e49f26e9d300f",
+    "url": "/TidalPrintingSite/static/media/stacked_cards.223f5d48.png"
+  },
+  {
+    "revision": "68cd63718779e4d215f27ad82a80e2db",
+    "url": "/TidalPrintingSite/static/media/track.68cd6371.svg"
+  }
+]);
